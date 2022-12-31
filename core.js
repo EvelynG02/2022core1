03bf3743b2b4c4d1787ebeece2562a20f2a3665d
EvelynG02 <90287881+EvelynG02@users.noreply.github.com>
2022-12-31 18:49:27 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/EvelynG02/2022core1""
    This reverts commit 8bd10e57674d19b1ac8c9ad8b235adf849a8a871.

--- core.js
+++ core.js
@@ -15,3 +15,51 @@ function changeSide() {
 changeSide();
 
 radioGroup.addEventListener( 'change', changeSide );
+
+var addinghello = setInterval(function(){$("body").prepend('<div class="box1"><div>' ); }, 500)
+$("#prepend-btn").click(function(){
+    $("body").prepend('<div class="newbox"><div>' )
+    });
+
+$("#append-btn").click(function(){
+    $("body").append('<div class="newbox"><div>' )
+    });
+
+
+   
+
+var addinghello = setInterval(function(){$("body").append('<div class="newbox"><div>' )}, 1000);
+
+
+
+$("stop").click(function(){
+clearInterval(addinghello);
+});
+
+$("colorchange").click(function(){
+    setTimeout(function()
+    {$("body").css({"background-color":"red"})}, 5000);
+    });
+
+
+console.log($("#div1").width());/*60*/
+console.log($("#div2").width());/*100*/
+console.log($("#div1").outerWidth());/*100*/
+console.log($("#div2").outerWidth());/*140*/
+
+
+$("#div1").width(500);/*set the width of #div1 to 540px, so the width without border and padding is 500px*/
+$("#div2").width(500);/*set the width of #div1 to 500px, so the width without border and padding is 500px*/
+
+
+$("#div1").outerWidth(100);/*set the width of #div1 to 500px, so the width with border and padding is 500px*/
+$("#div2").outerWidth(500);/*set the width of #div1 to 460px, so the width with border and padding is 500px*/
+
+var div2width = $("#div2").css("width");/*get the width of #div2, and store the value in the variable div2width*/
+var div2BackgroundColor = $("#div2").css("background-color");/*get the background-color of #div2, and store the value in the variable div2BackgroundColor*/
+
+console.log("width of div2: " + div2width);
+console.log("background-color of div2: " + div2BackgroundColor);
+
+
+$("#div2").css({"background-color": "purple", "width": "20%", "height": "20%"});
